abb6e9508e271bbeec2f69fea97932a29238e7e8
svalvastic <assogames2@gmail.com>
2022-08-29 18:54:56 +0200

    ✨ feat(script): add hover and click
    when mouse enter in preview, full image change  when click image is pinned else when mouse exit return previous image.

--- javascript/script.js
+++ javascript/script.js
@@ -1,54 +1,62 @@
-
-let allImages = ["01", "02", "03", "04", "05"]
+let allImages = ["01", "02", "03", "04", "05"];
 let currentImage = 0;
-let previewImage = []
-
-console.log(currentImage.toFixed(1))
-
-
+let previewImage = [];
+const image = document.getElementById("imageFull");
 const imgRaccolta = document.querySelector(".img-raccolta");
-for (let i = 0; i < allImages.length; i++) {
-    const preview = document.createElement("img");
-    preview.setAttribute("src", `img/${allImages[i]}.jpg`)
-    if (i === 0) {
-        preview.classList.add("active")
-    }
-    previewImage.push(preview)
-    imgRaccolta.appendChild(preview)
-   
+
+function changeImage(value) {
+  image.setAttribute("src", `img/${allImages[value]}.jpg`);
+  previewImage[currentImage].classList.remove("active");
+  currentImage = value;
+  previewImage[value].classList.add("active");
 }
 
-console.log(previewImage)
+for (let i = 0; i < allImages.length; i++) {
+  const preview = document.createElement("img");
+  preview.setAttribute("src", `img/${allImages[i]}.jpg`);
+  if (i === 0) {
+    preview.classList.add("active");
+  }
+
+  preview.addEventListener("mouseenter", () => {
+    let previous = currentImage;
+    let check = false;
+    changeImage(i);
+    preview.addEventListener("mouseleave", () => {
+      if (!check) changeImage(previous);
+    });
+    preview.addEventListener("click", () => {
+      changeImage(i);
+      check = true;
+    });
+  });
+
+  previewImage.push(preview);
+  imgRaccolta.appendChild(preview);
+}
+
+console.log(previewImage);
 
 // DOWNARROW
-const downArrow = document.getElementById("downArrow")
+const downArrow = document.getElementById("downArrow");
 downArrow.addEventListener("click", () => {
-    previewImage[currentImage].classList.remove("active")
+  let futureImage = currentImage;
 
-    if (currentImage === (allImages.length -1)) {
-        currentImage = 0;
-    }
-    else {
-        currentImage++
-    }
-
-    previewImage[currentImage].classList.add("active")
-    const image = document.getElementById("imageFull")
-    image.setAttribute("src", `img/${allImages[currentImage]}.jpg`)
-
-})
+  if (currentImage === allImages.length - 1) {
+    futureImage = 0;
+  } else {
+    futureImage++;
+  }
+  changeImage(futureImage);
+});
 
 //UPARROW
 upArrow.addEventListener("click", () => {
-    previewImage[currentImage].classList.remove("active")
-    if (currentImage === 0) {
-        currentImage = allImages.length -1;
-    }
-    else {
-        currentImage--
-    }
-    previewImage[currentImage].classList.add("active")
-    const image = document.getElementById("imageFull")
-    image.setAttribute("src", `img/${allImages[currentImage]}.jpg`)
-
-})
+  let futureImage = currentImage;
+  if (currentImage === 0) {
+    futureImage = allImages.length - 1;
+  } else {
+    futureImage--;
+  }
+  changeImage(futureImage);
+});
